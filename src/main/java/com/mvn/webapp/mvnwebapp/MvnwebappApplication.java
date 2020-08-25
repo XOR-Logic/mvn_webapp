f4dde02d5c444afd4146ead0251594f6be1a30ba
unknown <agungfatwa82@yahoo.co.id>
2020-08-25 21:06:50 +0700

    initial springboot webapp with maven
    initial springboot webapp with maven

---
+++ src/main/java/com/mvn/webapp/mvnwebapp/MvnwebappApplication.java
@@ -0,0 +1,13 @@
+package com.mvn.webapp.mvnwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvn/webapp/mvnwebapp/MvnwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvn.webapp.mvnwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
